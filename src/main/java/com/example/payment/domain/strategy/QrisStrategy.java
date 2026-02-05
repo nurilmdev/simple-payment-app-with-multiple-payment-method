982928e40c53ec28a1821eb10d2ca76a9900f5ba
wo-nurilM <nurilmustofaa18@gmail.com>
2026-02-05 17:17:33 +0700

    add feature qris payment method

---
+++ src/main/java/com/example/payment/domain/strategy/QrisStrategy.java
@@ -0,0 +1,22 @@
+package com.example.payment.domain.strategy;
+
+import com.example.payment.domain.PaymentMethod;
+import com.example.payment.domain.PaymentStatus;
+import com.example.payment.entity.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class QrisStrategy implements PaymentStrategy{
+    @Override
+    public String getType() {
+        return PaymentMethod.QRIS.name();
+    }
+
+    @Override
+    public void pay(Payment payment) {
+        log.info("Generate QR Code");
+        payment.setStatus(PaymentStatus.PENDING);
+    }
+}
