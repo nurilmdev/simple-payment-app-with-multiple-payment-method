529ed99cfd1d3622d85798f823faa4ec80a1c531
wo-nurilM <nurilmustofaa18@gmail.com>
2026-02-06 09:33:34 +0700

    add catch exception handler for illegal argument request

--- src/main/java/com/example/payment/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/payment/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.payment.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -38,6 +39,26 @@ public class GlobalExceptionHandler {
 
     }
 
+    // 400 - illegal input error
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidation(
+            IllegalArgumentException ex,
+            HttpServletRequest request) {
+        String msg = "";
+        if(ex.getMessage().contains("No enum constant com.example.payment.domain.PaymentMethod"))
+            msg = "Payment Method Doesn't Exists";
+        log.error(ex.getMessage());
+        return ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(400)
+                .error("Invalid Request")
+                .message(msg)
+                .path(request.getRequestURI())
+                .build();
+
+    }
+
     // 404
     @ExceptionHandler(PaymentNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
@@ -54,6 +75,24 @@ public class GlobalExceptionHandler {
                 .build();
     }
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleNotFound(
+            DataIntegrityViolationException ex,
+            HttpServletRequest request) {
+        log.error(ex.getMessage());
+        String msg = "";
+        if(ex.getMostSpecificCause().getMessage().contains("uk_reference_number"))
+            msg = "Payment with same reference number already exists";
+        return ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.CONFLICT.value())
+                .error("Duplicate Data")
+                .message(msg)
+                .path(request.getRequestURI())
+                .build();
+    }
+
     // 500 fallback
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -61,6 +100,7 @@ public class GlobalExceptionHandler {
             Exception ex,
             HttpServletRequest request) {
         log.error(ex.getMessage());
+        ex.printStackTrace();
         return ErrorResponse.builder()
                 .timestamp(LocalDateTime.now())
                 .status(500)
